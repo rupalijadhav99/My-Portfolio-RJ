c9b3777f0c8d3f7c6638af093423ce56bba7dc2a
Rupali Jadhav <Rupali Jadhav@ho.rnt.ai>
2025-04-29 14:55:14 +0530

    Resolving issue of css file

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -7,7 +7,7 @@ function ProjectCard({ name, description, features, technologies, imageUrl }) {
 
   return (
     <motion.div
-      className="enhanced-card new-card-layout"
+      className="new-card-layout"
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
